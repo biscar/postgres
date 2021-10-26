04929ce736892919d1d2bdfde7e08a65546a67ba
Vitali Stukau <Vitali.Stukau@itechart-group.com>
2021-10-26 17:41:53 +0300

    Added RANK DENSE example.

--- window_functions.sql
+++ window_functions.sql
@@ -37,3 +37,16 @@ with tennis_goldf as (
 select name as champion, sex, year, lead(name) over (partition by sex order by year asc) as future_champion
 from tennis_goldf
 order by sex asc, year asc;
+
+/* RANK DENSE*/
+
+with countries as (
+    select noc, count(distinct year) as participated
+    from athlete_events where noc in ('GBR', 'DEN', 'FRA', 'ITA','BLR')
+    group by noc
+)
+select noc, participated,
+       row_number() over (order by participated desc),
+       rank() over (order by participated desc),
+       dense_rank() over  (order by participated desc)
+from countries order by participated desc;
