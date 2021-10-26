cfe36c9c294d30382388d7fe5f04f98b2c459bf3
Vitali Stukau <Vitali.Stukau@itechart-group.com>
2021-10-26 17:23:33 +0300

    Added lead and lag example.

--- window_functions.sql
+++ window_functions.sql
@@ -5,4 +5,35 @@ from athlete_events order by row_number asc;
 select sport, row_number() over (order by sport asc ) as row_n
 from ( select distinct sport from athlete_events) as sports order by sport asc;
 
+/* Lag, PARTITION BY */
+with tennis_gold as (
+    select name, sex, year, noc
+    from athlete_events
+    where year >= 2004
+      and sport = 'Tennis'
+      and event like '%Singles%'
+      and medal = 'Gold'
+)
 
+select name as champion, sex, year, lead(name) over (partition by sex order by year asc) as future_champion
+from tennis_gold
+order by sex asc, year asc;
+
+select name as champion, sex, year, lag(name) over (partition by sex order by year asc) as last_champion
+from tennis_gold
+order by sex asc, year asc;
+
+/* LEAD */
+
+with tennis_goldf as (
+    select name, sex, year, noc
+    from athlete_events
+    where year >= 2004
+      and sport = 'Tennis'
+      and event like '%Singles%'
+      and medal = 'Gold'
+)
+
+select name as champion, sex, year, lead(name) over (partition by sex order by year asc) as future_champion
+from tennis_goldf
+order by sex asc, year asc;
