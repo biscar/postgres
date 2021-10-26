19ce707dbace5f74f936fc9f0666323371fea764
Vitali Stukau <Vitali.Stukau@itechart-group.com>
2021-10-26 16:55:40 +0300

    Added ROW_NUMBER и ORDER BY example.

--- window_functions.sql
+++ window_functions.sql
@@ -1,3 +1,8 @@
 /* ROW_NUMBER и ORDER BY */
 select name, event, row_number() over () as row_number
 from athlete_events order by row_number asc;
+
+select sport, row_number() over (order by sport asc ) as row_n
+from ( select distinct sport from athlete_events) as sports order by sport asc;
+
+
